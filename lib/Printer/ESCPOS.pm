d7ae1c4375a8a013890c467d9c07df25e0614f92
Shantanu Bhadoria <shantanu@cpan.org>
2015-03-09 18:16:06 +0800

    POD data updates

--- lib/Printer/ESCPOS.pm
+++ lib/Printer/ESCPOS.pm
@@ -162,46 +162,41 @@ __END__
 
 = SYNOPSIS
 
-  use Printer::ESCPOS;
+Printer::ESCPOS provides four different types of printer connections to talk to a ESCPOS printer. 
+Among these connection types 'Serial', 'Network', 'File' are already implemented in this module. 'USB' type using [Device::USB] module is under development. In the meantime most of the printing tasks for USB connected printer can be achieved using the 'File' connection mode for usb connected ESCPOS printers.
 
-  #For Network Printers $port is 9100 in most cases but might differ depending on how you have configured your printer
-  $printer = Printer::ESCPOS->new(device_ip=>$printer_ip,device_port=>$port);
+    use Printer::ESCPOS;
 
-  #These commands won't actually send anything to the printer but it will store all the merged data including control codes to send to printer in $printer->print_string variable.
-  $printer->write("Blah Blah \nReceipt Details\nFooter");
-  $printer->bold_on();
-  $printer->write("Bold Text");
-  $printer->bold_off();
-  $printer->print(); ##Sends the above set of code to the printer. Clears the buffer text in module.
-  
-  #For local printer connected on serial port, check syslog(Usually under /var/log/syslog) for what device file was created for your printer when you connect it to your system(For plug and play printers).
-  my $path = '/dev/ttyACM0';
-  $printer = Printer::ESCPOS->new(serial_device_path=$path);
-  $printer->write("Blah Blah \nReceipt Details\nFooter");
-  $printer->bold_on();
-  $printer->write("Bold Text");
-  $printer->bold_off();
-  $printer->print();
+    #For Network Printers $port is 9100 in most cases but might differ depending on how you have configured your printer
+    $printer = Printer::ESCPOS->new(device_ip=>$printer_ip,device_port=>$port);
 
-  #For local printer connected on usb port, check syslog(Usually under /var/log/syslog) for what device file was created for your printer when you connect it to your system(For plug and play printers).
-  my $path = '/dev/usb/lp0';
-  $printer = Printer::ESCPOS->new(usb_device_path=$path);
-  $printer->write("Blah Blah \nReceipt Details\nFooter");
-  $printer->bold_on();
-  $printer->write("Bold Text");
-  $printer->bold_off();
-  $printer->print();
+    #These commands won't actually send anything to the printer but it will store all the merged data including control codes to send to printer in $printer->print_string variable.
+    $printer->write("Blah Blah \nReceipt Details\nFooter");
+    $printer->bold_on();
+    $printer->write("Bold Text");
+    $printer->bold_off();
+    $printer->print(); ##Sends the above set of code to the printer. Clears the buffer text in module.
+
+    #For local printer connected on serial port, check syslog(Usually under /var/log/syslog) for what device file was created for your printer when you connect it to your system(For plug and play printers).
+    my $path = '/dev/ttyACM0';
+    $device = Printer::ESCPOS->new(serial_device_path=$path);
+    $device->printer->write("Blah Blah \nReceipt Details\nFooter");
+    $printer->bold_on();
+    $printer->write("Bold Text");
+    $printer->bold_off();
+    $printer->print();
+
 
 = DESCRIPTION
 
-You can use this module for all your ESC-POS Printing needs. If some of your printer's functions are not included, you can even extend this module by adding specialized funtions for your printer in it's own subclass.
+You can use this module for all your ESC-POS Printing needs. If some of your printer's functions are not included, you may extend this module by adding specialized funtions for your printer in it's own subclass. Refer to [Printer::ESCPOS::Roles::Profile] and [Printer::ESCPOS::Profiles::Generic]
 
-For ESC-P codes refer the guide from Epson http://support.epson.ru/upload/library_file/14/esc-p.pdf
 
 = NOTES
 
 * If the printer prints out garbled characters instead of proper text, try specifying the baudrate parameter when creating printer object when you create the printer object(not for network or USB printers)
     $printer = Printer::ESCPOS->new(serial_device_path => '/dev/ttyACM0', baudrate => 9600);
+* For ESC-P codes refer the guide from Epson http://support.epson.ru/upload/library_file/14/esc-p.pdf
 
 = USAGE
 
