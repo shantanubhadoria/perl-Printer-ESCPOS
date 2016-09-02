4426af4b3ef3e82b60d6bd36b95269a1c1e8b715
Shantanu Bhadoria <shantanu@cpan.org>
2016-09-02 18:25:02 +0800

    utf8 in doc!

--- lib/Printer/ESCPOS/Profiles/Generic.pm
+++ lib/Printer/ESCPOS/Profiles/Generic.pm
@@ -142,7 +142,7 @@ sub qr {
 
     use utf8;
 
-    $device->printer->utf8ImagedText("शांतनु भदौरिया",
+    $device->printer->utf8ImagedText("Hello World\x{8A9E}",
       fontFamily => "Rubik",
       fontStyle => "Normal",
       fontSize => 25,
